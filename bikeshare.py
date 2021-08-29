722099de5092cb0bb41f741fa1882736f47dfee5
Jesse Ballesil <gb2856@att.com>
2021-08-28 20:53:02 -0500

    Add inline header documentation

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,11 @@ import time
 import pandas as pd
 import numpy as np
 
+"""
+The following are static data structures based on available data and
+granulation of data analysis.
+"""
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
